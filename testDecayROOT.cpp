d497ebcc7e41243a54c7b00340e35a0ebae15e10
Davide Mancusi <davide.mancusi@cea.fr>
2015-01-29 14:00:00 +0100

    Refine testDecayROOT

--- testDecayROOT.cpp
+++ testDecayROOT.cpp
@@ -1,5 +1,6 @@
 #include "CNucleus.h"
 #include "CAngle.h"
+#include "CRandom.h"
 #include <iostream>
 #include <sstream>
 #include "TFile.h"
@@ -10,8 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-  if(argc!=6) {
-    std::cerr << "Syntax: testDecay Z A EStar J nEvents" << std::endl;
+  if(argc<6) {
+    std::cerr << "Syntax: testDecay Z A EStar J nEvents [vx vy vz]" << std::endl;
     return 1;
   }
   std::stringstream ss;
@@ -40,14 +41,29 @@ int main(int argc, char *argv[])
 
   CN.setCompoundNucleus(fEx,fJ); //specify the excitation energy and spin
 
-  CN.setVelocityCartesian(); // set initial CN velocity to zero
-  CAngle spin(CNucleus::pi/2,(float)0.); 
-  CN.setSpinAxis(spin); //set the direction of the CN spin vector
-
   int nEvents; // number of events
   ss.str(argv[5]);
   ss >> nEvents;
   ss.clear();
+
+  float vx=0.0, vy=0.0, vz=0.0; // velocity
+  if(argc>6) {
+    ss.str(argv[6]);
+    ss >> vx;
+    ss.clear();
+  }
+  if(argc>7) {
+    ss.str(argv[7]);
+    ss >> vy;
+    ss.clear();
+  }
+  if(argc>8) {
+    ss.str(argv[8]);
+    ss >> vz;
+    ss.clear();
+  }
+
+  CN.setVelocityCartesian(vx, vy, vz); // set initial CN velocity to zero
 
   const int maxParticles = 300;
   Short_t nParticles, A[maxParticles], Z[maxParticles];
@@ -67,6 +83,13 @@ int main(int argc, char *argv[])
   for (int i=0;i<nEvents;i++) {
     if(i%1000==0)
       cout << "event = " << i << endl;
+    const float thetaJ = std::acos(1.-2.*CRandom::Rndm());
+    const float phiJ = CAngle::pi*2.*CRandom::Rndm();
+    CAngle spin(thetaJ, phiJ);
+    std::cout << spin.theta << '\t' << spin.phi << std::endl;
+    //  CAngle spin((float)0.,(float)0.);
+    CN.setSpinAxis(spin); //set the direction of the CN spin vector
+
     //CN.setWeightIMF();// turn on enhanced IMF emission
     CN.decay(); //decay the compound nucleus
 
